18664a214151863d7315d84362699da950a10dfd
LONG LE <96783174+LongLeGL@users.noreply.github.com>
2024-12-22 10:52:48 +0700

    Create timestampMicroservice.js

---
+++ backendAndApiProjects/timestampMicroservice.js
@@ -0,0 +1,55 @@
+// index.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your API endpoints... 
+app.get("/api/", function (req, res) {
+  let currDate = new Date();
+  res.json({
+    "unix":currDate.getTime(), 
+    "utc": currDate.toUTCString()
+  });
+});
+
+app.get("/api/:date", function(req, res){
+  let param = req.params.date;
+  let dateObj = null;
+  console.log("Got input:", param)
+  if (Number(param))
+    dateObj = new Date(Number(param));
+  else
+    dateObj = new Date(param);
+
+  if (isNaN(dateObj)){
+    res.json({ error : "Invalid Date" });
+  }
+  else{
+    res.json({
+      "unix":dateObj.getTime(), 
+      "utc": dateObj.toUTCString()
+    });
+  }
+});
+
+
+// Listen on port set in environment variable or default to 3000
+var listener = app.listen(process.env.PORT || 3000, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
